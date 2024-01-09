45c4e6e1050a0a5311f87a8b6bdeb1aa3c07b2fd
Edith Thaba <writersherry5@gmail.com>
2024-01-09 17:47:28 +0300

    Add Event Listener to dropdown

--- src/index.js
+++ src/index.js
@@ -38,5 +38,20 @@ fetch(imgUrl)
     
     })
     .catch(error => console.error('Error fetching dog breeds:', error));
+    //Challenge 3
+    
+    //Challenge 4
+        // Add event listener to the dropdown for breed filtering
+        dropdown.addEventListener('change', () => {
+            const selectedLetter = dropdown.value.toLowerCase();
+
+            // Show breeds that start with the selected letter and hide the others
+            const breedListItems = dogBreedContainer.getElementsByTagName('li');
+            for (const breedListItem of breedListItems) {
+                const firstLetter = breedListItem.getAttribute('data-letter');
+                breedListItem.style.display = firstLetter === selectedLetter ? 'block' : 'none';
+            }
+            
+        });
 
     
